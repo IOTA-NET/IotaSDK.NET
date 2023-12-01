36752ed330428bd82b4fcf020a5635cdf91fdc5e
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-01 18:33:58 +0800

    Added AliasIdToBech32

--- IotaSDK.NET/Common/Interfaces/IIotaUtilities.cs
+++ IotaSDK.NET/Common/Interfaces/IIotaUtilities.cs
@@ -13,5 +13,7 @@ namespace IotaSDK.NET.Common.Interfaces
         Task<IotaSDKResponse<string>> ConvertNftIdToBech32Async(string nftId, HumanReadablePart humanReadablePart);
 
         Task<IotaSDKResponse<string>> ConvertBech32ToHexStringAsync(string bech32Address);
+
+        Task<IotaSDKResponse<string>> ConvertAliasIdToBech32Async(string aliasId, HumanReadablePart humanReadablePart);
     }
 }
---
+++ IotaSDK.NET/Contexts/UtilsContext/Commands/AliasIdToBech32/AliasIdToBech32CommandModelData.cs
@@ -0,0 +1,14 @@
+﻿namespace IotaSDK.NET.Contexts.UtilsContext.Commands.AliasIdToBech32
+{
+    internal class AliasIdToBech32CommandModelData
+    {
+        public AliasIdToBech32CommandModelData(string aliasId, string bech32HumanReadablePart)
+        {
+            AliasId = aliasId;
+            Bech32Hrp = bech32HumanReadablePart;
+        }
+
+        public string AliasId { get; }
+        public string Bech32Hrp { get; }
+    }
+}
