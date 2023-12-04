e893e6e8e50264bbbecf60adf5fdd2c5e41ec1d9
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-04 20:17:16 +0800

    Added Publickey to bech32

---
+++ IotaSDK.NET/Contexts/UtilsContext/Commands/Bech32ToHash/Bech32ToHashCommandModelData.cs
@@ -0,0 +1,12 @@
+﻿namespace IotaSDK.NET.Contexts.UtilsContext.Commands.Bech32ToHash
+{
+    internal class Bech32ToHashCommandModelData
+    {
+        public Bech32ToHashCommandModelData(string bech32)
+        {
+            Bech32 = bech32;
+        }
+
+        public string Bech32 { get; }
+    }
+}
---
+++ IotaSDK.NET/Contexts/UtilsContext/Commands/PublicKeyToBech32/PublicKeyToBech32CommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using IotaSDK.NET.Common.Exceptions;
+using IotaSDK.NET.Common.Interfaces;
+using IotaSDK.NET.Common.Rust;
+using IotaSDK.NET.Domain.Network;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IotaSDK.NET.Contexts.UtilsContext.Commands.PublicKeyToBech32
+{
+    internal class PublicKeyToBech32CommandHandler : IRequestHandler<PublicKeyToBech32Command, IotaSDKResponse<string>>
+    {
+        private readonly RustBridgeCommon _rustBridgeCommon;
+
+        public PublicKeyToBech32CommandHandler(RustBridgeCommon rustBridgeCommon)
+        {
+            _rustBridgeCommon = rustBridgeCommon;
+        }
+
+        public async Task<IotaSDKResponse<string>> Handle(PublicKeyToBech32Command request, CancellationToken cancellationToken)
+        {
+            PublicKeyToBech32CommandModelData modelData = new PublicKeyToBech32CommandModelData(request.HexEncodedPublicKey, HumanReadablePartEnumConverter.Convert(request.HumanReadablePart));
+            IotaSDKModel<PublicKeyToBech32CommandModelData> model = new IotaSDKModel<PublicKeyToBech32CommandModelData>("hexPublicKeyToBech32Address", modelData);
+            string json = model.AsJson();
+
+            string? callUtilsResponse = await _rustBridgeCommon.CallUtilsMethodAsync(json);
+
+            IotaSDKException.CheckForException(callUtilsResponse!);
+
+            return IotaSDKResponse<string>.CreateInstance(callUtilsResponse);
+        }
+    }
+}
