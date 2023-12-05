052d01526a80643ae14d87e5598584eb14b1dcfb
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-05 23:33:27 +0800

    Create OutputType.cs

---
+++ IotaSDK.NET/Domain/Outputs/OutputType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IotaSDK.NET.Domain.Outputs
+{
+
+    /**
+     * All of the output types.
+     */
+    public enum OutputType
+    {
+        /** A Treasury output. */
+        Treasury = 2,
+        /** A Basic output. */
+        Basic = 3,
+        /** An Alias output. */
+        Alias = 4,
+        /** A Foundry output. */
+        Foundry = 5,
+        /** An NFT output. */
+        Nft = 6,
+    }
+}
