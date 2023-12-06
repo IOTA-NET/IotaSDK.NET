09e045e2460a2282fb2fc19ed6fd83d0e4187e83
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-06 23:57:56 +0800

    Create ImmutableAliasAddressUnlockCondition.cs

---
+++ IotaSDK.NET/Domain/UnlockConditions/ImmutableAliasAddressUnlockCondition.cs
@@ -0,0 +1,20 @@
+﻿using IotaSDK.NET.Domain.Addresses;
+
+namespace IotaSDK.NET.Domain.UnlockConditions
+{
+    /**
+     * An Immutable Alias Address Unlock Condition.
+     */
+    public class ImmutableAliasAddressUnlockCondition : UnlockCondition
+    {
+        public ImmutableAliasAddressUnlockCondition(int type, Address address) : base(type)
+        {
+            Address = address;
+        }
+
+        /// <summary>
+        /// The Immutable Alias address that owns the output.
+        /// </summary>
+        public Address Address { get; }
+    }
+}
