8f4aad2ee79086b843185215edd23fe2b1039835
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-07 00:18:10 +0800

    Create IssuerFeature.cs

---
+++ IotaSDK.NET/Domain/Features/IssuerFeature.cs
@@ -0,0 +1,20 @@
+﻿using IotaSDK.NET.Domain.Addresses;
+
+namespace IotaSDK.NET.Domain.Features
+{
+    /// <summary>
+    /// An Issuer feature.
+    /// </summary>
+    public class IssuerFeature : Feature
+    {
+        public IssuerFeature(int type, Address address) : base(type)
+        {
+            Address = address;
+        }
+
+        /// <summary>
+        /// The Issuer address stored with the feature.
+        /// </summary>
+        public Address Address { get; }
+    }
+}
