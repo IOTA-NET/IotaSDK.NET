3451fadaf4f0c886f023e7f2b7878500cd90976b
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-07 00:15:52 +0800

    Create FeatureType.cs

---
+++ IotaSDK.NET/Domain/Features/FeatureType.cs
@@ -0,0 +1,19 @@
+﻿using IotaSDK.NET.Domain.UnlockConditions;
+
+namespace IotaSDK.NET.Domain.Features
+{
+    /**
+     * All of the feature block types.
+     */
+    public enum FeatureType
+    {
+        /** A Sender feature. */
+        Sender = 0,
+        /** An Issuer feature. */
+        Issuer = 1,
+        /** A Metadata feature. */
+        Metadata = 2,
+        /** A Tag feature. */
+        Tag = 3,
+    }
+}
