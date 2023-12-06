b48b854c68c46a650587764ce4fb4fee2f2600c1
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-07 01:08:14 +0800

    Create AliasOutput.cs

---
+++ IotaSDK.NET/Domain/Outputs/AliasOutput.cs
@@ -0,0 +1,37 @@
+﻿using IotaSDK.NET.Domain.UnlockConditions;
+using System.Collections.Generic;
+
+namespace IotaSDK.NET.Domain.Outputs
+{
+    public class AliasOutput : StateMetadataOutput
+    {
+        public AliasOutput(
+            string amount, 
+            int type,
+            string aliasId,
+            List<UnlockCondition> unlockConditions, 
+            string? stateMetadata,
+            int foundryCounter,
+            int stateIndex) 
+            : base(amount, type, unlockConditions, stateMetadata)
+        {
+            AliasId = aliasId;
+            FoundryCounter = foundryCounter;
+            StateIndex = stateIndex;
+        }
+        /// <summary>
+        /// The Alias ID as hex-encoded string.
+        /// </summary>
+        public string AliasId { get; }
+
+        /// <summary>
+        /// A counter that denotes the number of foundries created by this alias account.
+        /// </summary>
+        public int FoundryCounter { get; }
+
+        /// <summary>
+        ///  A counter that must increase by 1 every time the alias is state transitioned.
+        /// </summary>
+        public int StateIndex { get; }
+    }
+}
