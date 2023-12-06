4aa2555d8af735cd9ab5b5c5a93765647421f877
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-07 00:15:54 +0800

    Create Feature.cs

---
+++ IotaSDK.NET/Domain/Features/Feature.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IotaSDK.NET.Domain.Features
+{
+    public abstract class Feature
+    {
+        public Feature(int type)
+        {
+            Type = type;
+        }
+
+        public int Type { get; }
+
+        public FeatureType GetFeatureType()
+        {
+            if (Enum.IsDefined(typeof(FeatureType), this.Type))
+            {
+                return (FeatureType)this.Type;
+            }
+            else
+            {
+                // Handle the case where the input value doesn't match any enum value
+                throw new ArgumentOutOfRangeException(nameof(this.Type), "Invalid FeatureType value");
+            }
+        }
+    }
+}
