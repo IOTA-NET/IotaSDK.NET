d3e764107a257bd23acc6bcb4b481b263e2c5417
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-06 22:39:01 +0800

    Added unlock conditions

---
+++ IotaSDK.NET/Domain/UnlockConditions/UnlockCondition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IotaSDK.NET.Domain.UnlockConditions
+{
+    public abstract class UnlockCondition
+    {
+        public UnlockCondition(int type)
+        {
+            Type = type;
+        }
+
+        public int Type { get; }
+
+        public UnlockConditionType GetUnlockConditionType()
+        {
+            if (Enum.IsDefined(typeof(UnlockConditionType), this.Type))
+            {
+                return (UnlockConditionType)this.Type;
+            }
+            else
+            {
+                // Handle the case where the input value doesn't match any enum value
+                throw new ArgumentOutOfRangeException(nameof(this.Type), "Invalid UnlockConditionType value");
+            }
+        }
+    }
+}
