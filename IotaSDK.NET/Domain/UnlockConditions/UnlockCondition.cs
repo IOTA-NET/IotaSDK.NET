2f0cf4de33db38967a1904f218bedacd1908d794
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-07 00:25:41 +0800

    Added JsonSubtypes

--- IotaSDK.NET/Domain/UnlockConditions/UnlockCondition.cs
+++ IotaSDK.NET/Domain/UnlockConditions/UnlockCondition.cs
@@ -1,7 +1,17 @@
-﻿using System;
+﻿using JsonSubTypes;
+using Newtonsoft.Json;
+using System;
 
 namespace IotaSDK.NET.Domain.UnlockConditions
 {
+    [JsonConverter(typeof(JsonSubtypes), "Type")]
+    [JsonSubtypes.KnownSubType(typeof(AddressUnlockCondition), 0)]
+    [JsonSubtypes.KnownSubType(typeof(StorageDepositReturnUnlockCondition), 1)]
+    [JsonSubtypes.KnownSubType(typeof(TimelockUnlockCondition), 2)]
+    [JsonSubtypes.KnownSubType(typeof(ExpirationUnlockCondition), 3)]
+    [JsonSubtypes.KnownSubType(typeof(StateControllerAddressUnlockCondition), 4)]
+    [JsonSubtypes.KnownSubType(typeof(GovernorAddressUnlockCondition), 5)]
+    [JsonSubtypes.KnownSubType(typeof(ImmutableAliasAddressUnlockCondition), 6)]
     public abstract class UnlockCondition
     {
         public UnlockCondition(int type)
