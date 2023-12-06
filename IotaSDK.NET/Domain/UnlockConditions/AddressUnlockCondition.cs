47a55862d233e611bd4f61a68c68b65c81e85e4b
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-06 23:36:12 +0800

    Update AddressUnlockCondition.cs

--- IotaSDK.NET/Domain/UnlockConditions/AddressUnlockCondition.cs
+++ IotaSDK.NET/Domain/UnlockConditions/AddressUnlockCondition.cs
@@ -1,11 +1,21 @@
-﻿namespace IotaSDK.NET.Domain.UnlockConditions
+﻿using IotaSDK.NET.Domain.Addresses;
+
+namespace IotaSDK.NET.Domain.UnlockConditions
 {
+    /**
+     * An address unlock condition.
+     * An address unlocked with a private key.
+     */
     public class AddressUnlockCondition : UnlockCondition
     {
-        public AddressUnlockCondition(int type) : base(type)
+        public AddressUnlockCondition(int type, Address address) : base(type)
         {
+            Address = address;
         }
 
-
+        /**
+         * @param address The address that needs to be unlocked with a private key.
+         */
+        public Address Address { get; }
     }
 }
