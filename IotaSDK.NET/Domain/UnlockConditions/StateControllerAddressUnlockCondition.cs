32c964c76e883aedc24ab4f6fb5170ab27b068d0
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-06 23:55:07 +0800

    Create StateControllerAddressUnlockCondition.cs

---
+++ IotaSDK.NET/Domain/UnlockConditions/StateControllerAddressUnlockCondition.cs
@@ -0,0 +1,20 @@
+﻿using IotaSDK.NET.Domain.Addresses;
+
+namespace IotaSDK.NET.Domain.UnlockConditions
+{
+    /// <summary>
+    /// A State Controller Address Unlock Condition.
+    /// </summary>
+    public class StateControllerAddressUnlockCondition : UnlockCondition
+    {
+        public StateControllerAddressUnlockCondition(int type, Address address) : base(type)
+        {
+            Address = address;
+        }
+
+        /// <summary>
+        /// The State Controller address that is allowed to do state transitions.
+        /// </summary>
+        public Address Address { get; }
+    }
+}
