68f06015e52a5974e32282fe5856b4dfc6bf3ada
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-07 01:38:16 +0800

    Create CommonOutput.cs

---
+++ IotaSDK.NET/Domain/Outputs/CommonOutput.cs
@@ -0,0 +1,26 @@
+﻿using IotaSDK.NET.Domain.Features;
+using IotaSDK.NET.Domain.Tokens;
+using IotaSDK.NET.Domain.UnlockConditions;
+using System.Collections.Generic;
+
+namespace IotaSDK.NET.Domain.Outputs
+{
+    public abstract class CommonOutput : Output
+    {
+        protected CommonOutput(string amount, 
+            int type, 
+            List<UnlockCondition> unlockConditions, 
+            List<NativeToken>? nativeTokens,
+            List<Feature>? features) 
+            : base(amount, type)
+        {
+            UnlockConditions = unlockConditions;
+            NativeTokens = nativeTokens;
+            Features = features;
+        }
+
+        public List<UnlockCondition> UnlockConditions { get; }
+        public List<NativeToken>? NativeTokens { get; }
+        public List<Feature>? Features { get; }
+    }
+}
