e66459195e65e4b1ff22dca724d3f7789958720c
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-07 00:33:10 +0800

    Create BasicOutput.cs

---
+++ IotaSDK.NET/Domain/Outputs/BasicOutput.cs
@@ -0,0 +1,16 @@
+﻿using IotaSDK.NET.Domain.UnlockConditions;
+using System.Collections.Generic;
+
+namespace IotaSDK.NET.Domain.Outputs
+{
+    public class BasicOutput : CommonOutput
+    {
+        public BasicOutput(
+            string amount, 
+            int type, 
+            List<UnlockCondition> unlockConditions) 
+            : base(amount, type, unlockConditions, null, null)
+        {
+        }
+    }
+}
