27e60fe6379062a927735a287e53d274415e0f22
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-07 02:42:31 +0800

    Create ComputeStorageDepositCommandModelData.cs

---
+++ IotaSDK.NET/Contexts/UtilsContext/Commands/ComputeStorageDeposit/ComputeStorageDepositCommandModelData.cs
@@ -0,0 +1,17 @@
+﻿using IotaSDK.NET.Domain.Outputs;
+using IotaSDK.NET.Domain.Tokens;
+
+namespace IotaSDK.NET.Contexts.UtilsContext.Commands.ComputeStorageDeposit
+{
+    internal class ComputeStorageDepositCommandModelData
+    {
+        public ComputeStorageDepositCommandModelData(Output output, Rent rent)
+        {
+            Output = output;
+            Rent = rent;
+        }
+
+        public Output Output { get; }
+        public Rent Rent { get; }
+    }
+}
