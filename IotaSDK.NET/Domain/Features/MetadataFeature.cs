f69c4b6dec629e898579214b4c6a38f4b7a7649d
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-07 00:20:35 +0800

    Create MetadataFeature.cs

---
+++ IotaSDK.NET/Domain/Features/MetadataFeature.cs
@@ -0,0 +1,18 @@
+﻿namespace IotaSDK.NET.Domain.Features
+{
+    /// <summary>
+    /// A Metadata feature.
+    /// </summary>
+    public class MetadataFeature : Feature
+    {
+        public MetadataFeature(int type, string data) : base(type)
+        {
+            Data = data;
+        }
+
+        /// <summary>
+        /// The metadata stored with the feature.
+        /// </summary>
+        public string Data { get; }
+    }
+}
