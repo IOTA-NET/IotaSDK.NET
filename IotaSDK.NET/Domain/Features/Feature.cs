cfe496141f71634e08136bdcc31878b1d718d841
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-07 00:27:12 +0800

    Added JsonSubtypes

--- IotaSDK.NET/Domain/Features/Feature.cs
+++ IotaSDK.NET/Domain/Features/Feature.cs
@@ -1,7 +1,15 @@
-﻿using System;
+﻿using IotaSDK.NET.Domain.Addresses;
+using JsonSubTypes;
+using Newtonsoft.Json;
+using System;
 
 namespace IotaSDK.NET.Domain.Features
 {
+    [JsonConverter(typeof(JsonSubtypes), "Type")]
+    [JsonSubtypes.KnownSubType(typeof(AliasAddress), (int)FeatureType.Sender)]
+    [JsonSubtypes.KnownSubType(typeof(Ed25519Address), (int)FeatureType.Issuer)]
+    [JsonSubtypes.KnownSubType(typeof(NftAddress), (int)FeatureType.Metadata)]
+    [JsonSubtypes.KnownSubType(typeof(NftAddress), (int)FeatureType.Tag)]
     public abstract class Feature
     {
         public Feature(int type)
