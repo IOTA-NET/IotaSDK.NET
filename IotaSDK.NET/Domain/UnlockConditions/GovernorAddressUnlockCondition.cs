200794f13ab7e4c931b16132c472b053da118717
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-06 23:56:31 +0800

    Create GovernorAddressUnlockCondition.cs

---
+++ IotaSDK.NET/Domain/UnlockConditions/GovernorAddressUnlockCondition.cs
@@ -0,0 +1,20 @@
+﻿using IotaSDK.NET.Domain.Addresses;
+
+namespace IotaSDK.NET.Domain.UnlockConditions
+{
+    /// <summary>
+    /// A Governor Address Unlock Condition.
+    /// </summary>
+    public class GovernorAddressUnlockCondition : UnlockCondition
+    {
+        public GovernorAddressUnlockCondition(int type, Address address) : base(type)
+        {
+            Address = address;
+        }
+
+        /// <summary>
+        /// The governor address that is allowed to do governance transitions.
+        /// </summary>
+        public Address Address { get; }
+    }
+}
