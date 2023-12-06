ded83605355809f7695f179688fabdff06d0453d
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-07 02:42:39 +0800

    Create ComputeStorageDepositCommandHandler.cs

---
+++ IotaSDK.NET/Contexts/UtilsContext/Commands/ComputeStorageDeposit/ComputeStorageDepositCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using IotaSDK.NET.Common.Interfaces;
+using IotaSDK.NET.Common.Rust;
+
+namespace IotaSDK.NET.Contexts.UtilsContext.Commands.ComputeStorageDeposit
+{
+    internal class ComputeStorageDepositCommandHandler : RustBridgeCommonHandler<ComputeStorageDepositCommand, IotaSDKResponse<string>, string, ComputeStorageDepositCommandModelData>
+    {
+        public ComputeStorageDepositCommandHandler(RustBridgeCommon rustBridgeCommon) : base(rustBridgeCommon)
+        {
+        }
+
+        public override ComputeStorageDepositCommandModelData CreateModelData(ComputeStorageDepositCommand request)
+        {
+            return new ComputeStorageDepositCommandModelData(request.Output, request.Rent);
+        }
+    }
+}
