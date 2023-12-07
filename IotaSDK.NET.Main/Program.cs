c9260b3810bb638edff751d19f679a126151db99
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-07 22:58:49 +0800

    Able to create a wallet

--- IotaSDK.NET.Main/Program.cs
+++ IotaSDK.NET.Main/Program.cs
@@ -18,25 +18,25 @@ namespace IotaSDK.NET.Main
 
             using (IServiceScope scope = serviceProvider.CreateScope())
             {
-                var iotaUtilities = scope.ServiceProvider.GetRequiredService<IIotaUtilities>();
+                var wallet = scope.ServiceProvider.GetRequiredService<IWallet>();
 
-                try
-                {
-                    Output basic = new BasicOutput("1000", new() { new AddressUnlockCondition(new Ed25519Address("0x40E9240E7A2AE86F7F25929C9D8B3248130D45EAB91C8CC8B03FE8EEC308073E")) });
-                    Rent rent = new Rent(1, 1, 1);
+                wallet = await wallet
+                            .ConfigureWalletOptions()
+                                .SetStoragePath("./storage")
+                                .SetCoinType(TypeOfCoin.Shimmer)
+                                .Then()
+                            .ConfigureClientOptions()
+                                .SetFaucetUrl("https://faucet.testnet.shimmer.network")
+                                .AddNodeUrl("https://api.testnet.shimmer.network")
+                                .IsLocalPow()
+                                .Then()
+                            .ConfigureSecretManagerOptions()
+                                .SetSnapshotPath("./snapshot")
+                                .SetPassword("password")
+                                .Then()
+                            .InitializeAsync();
 
-                    var r = await iotaUtilities.ComputeStorageDepositAsync(basic, rent);
-
-                    var bech32 = await iotaUtilities.ConvertAliasIdToBech32Async("0x86c9a6c9abbc0206498bd08517352b92a022f1b5f6596237a31761101d1fa447", Domain.Network.HumanReadablePart.Rms);
-                    Console.WriteLine(bech32);
-
-                    var hex = await iotaUtilities.VerifyBech32Address("asd");
-                    Console.WriteLine(hex);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.ToString());
-                }
+                int x = 34;
             }
         }
 
--- IotaSDK.NET/Wallet.cs
+++ IotaSDK.NET/Wallet.cs
@@ -1,13 +1,68 @@
-﻿using IotaSDK.NET.Common.Interfaces;
+﻿using IotaSDK.NET.Common.Exceptions;
+using IotaSDK.NET.Common.Interfaces;
+using IotaSDK.NET.Common.Rust;
 using IotaSDK.NET.Domain.Options;
+using IotaSDK.NET.Domain.Options.Builders;
+using MediatR;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
 
 namespace IotaSDK.NET
 {
     public class Wallet : IWallet
     {
+        private WalletOptions _walletOptions;
+        private IntPtr _walletHandle;
+        private IMediator _mediator;
+        private readonly RustBridgeWallet _rustBridgeWallet;
+        private readonly RustBridgeCommon _rustBridgeCommon;
+
+        public Wallet(IMediator mediator, RustBridgeWallet rustBridgeWallet, RustBridgeCommon rustBridgeCommon)
+        {
+            _walletOptions = new WalletOptions();
+
+            _walletHandle = IntPtr.Zero;
+
+            _mediator = mediator;
+            _rustBridgeWallet = rustBridgeWallet;
+            _rustBridgeCommon = rustBridgeCommon;
+        }
+        public async Task<IWallet> InitializeAsync()
+        {
+            string walletOptions = JsonConvert.SerializeObject(GetWalletOptions());
+
+            IntPtr? walletHandle = await _rustBridgeWallet.CreateWalletAsync(walletOptions);
+
+            if(walletHandle.HasValue == false)
+            {
+                string? error = await _rustBridgeCommon.GetLastErrorAsync();
+                throw new IotaSDKException($"Unable to create wallet.\nError:{error}");
+            }
+
+            _walletHandle = walletHandle!.Value;
+
+            return this;
+        }
+
+        public ClientOptionsBuilder ConfigureClientOptions()
+            => new ClientOptionsBuilder(this);
+
+        public SecretManagerOptionsBuilder ConfigureSecretManagerOptions()
+            => new SecretManagerOptionsBuilder(this);
+
+        public WalletOptionsBuilder ConfigureWalletOptions()
+            => new WalletOptionsBuilder(this);
+
         public WalletOptions GetWalletOptions()
         {
-            throw new System.NotImplementedException();
+            return _walletOptions;
+        }
+
+        public void Dispose()
+        {
+            _rustBridgeWallet.DestroyWalletAsync(_walletHandle).Wait();
+            _walletHandle = IntPtr.Zero;
         }
     }
 
