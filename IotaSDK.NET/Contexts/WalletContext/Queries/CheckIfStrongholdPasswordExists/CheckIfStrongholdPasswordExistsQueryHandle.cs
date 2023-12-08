c22f8a167b144477a764ffa6282ba68a94d8b7bc
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-08 20:01:39 +0800

    Create CheckIfStrongholdPasswordExistsQueryHandle.cs

---
+++ IotaSDK.NET/Contexts/WalletContext/Queries/CheckIfStrongholdPasswordExists/CheckIfStrongholdPasswordExistsQueryHandle.cs
@@ -0,0 +1,27 @@
+﻿using IotaSDK.NET.Common.Interfaces;
+using IotaSDK.NET.Common.Rust;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IotaSDK.NET.Contexts.WalletContext.Queries.CheckIfStrongholdPasswordExists
+{
+    internal class CheckIfStrongholdPasswordExistsQueryHandle : IRequestHandler<CheckIfStrongholdPasswordExistsQuery, bool>
+    {
+        private readonly RustBridgeWallet _rustBridgeWallet;
+
+        public CheckIfStrongholdPasswordExistsQueryHandle(RustBridgeWallet rustBridgeWallet)
+        {
+            _rustBridgeWallet = rustBridgeWallet;
+        }
+
+        public async Task<bool> Handle(CheckIfStrongholdPasswordExistsQuery request, CancellationToken cancellationToken)
+        {
+            IotaSDKModel model = new IotaSDKModel("isStrongholdPasswordAvailable");
+            string json = model.AsJson();
+            string? walletResponse =  await _rustBridgeWallet.CallWalletMethodAsync(request.WalletHandle, json);
+
+            return true;
+        }
+    }
+}
