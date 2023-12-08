5543df062671a89f92d0c3eabe094afdbf931e50
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-08 20:58:28 +0800

    Create AuthenticateToStrongholdCommand.cs

---
+++ IotaSDK.NET/Contexts/WalletContext/Commands/AuthenticateToStronghold/AuthenticateToStrongholdCommand.cs
@@ -0,0 +1,19 @@
+﻿using IotaSDK.NET.Common.Interfaces;
+using MediatR;
+using System;
+
+namespace IotaSDK.NET.Contexts.WalletContext.Commands.AuthenticateToStronghold
+{
+    internal class AuthenticateToStrongholdCommand : IRequest<IotaSDKResponse<bool>>
+    {
+        public AuthenticateToStrongholdCommand(IntPtr walletHandle, string password)
+        {
+            Password = password;
+            WalletHandle = walletHandle;
+
+        }
+        public IntPtr WalletHandle { get; }
+
+        public string Password { get; }
+    }
+}
