c67c3f1e58a911bddd925e613f7763495a95bf4a
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-09 18:22:24 +0800

    Create SetStrongholdPasswordClearIntervalCommand.cs

---
+++ IotaSDK.NET/Contexts/WalletContext/Commands/SetStrongholdPasswordClearInterval/SetStrongholdPasswordClearIntervalCommand.cs
@@ -0,0 +1,40 @@
+﻿using IotaSDK.NET.Common.Interfaces;
+using IotaSDK.NET.Common.Rust;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IotaSDK.NET.Contexts.WalletContext.Commands.SetStrongholdPasswordClearInterval
+{
+    internal class SetStrongholdPasswordClearIntervalCommand : IRequest<IotaSDKResponse<bool>>
+    {
+        public SetStrongholdPasswordClearIntervalCommand(int intervalInMilliSeconds)
+        {
+            IntervalInMilliSeconds = intervalInMilliSeconds;
+        }
+
+        public int IntervalInMilliSeconds { get; }
+    }
+
+    internal class SetStrongholdPasswordClearIntervalCommandModelData
+    {
+        public SetStrongholdPasswordClearIntervalCommandModelData(int intervalInMilliseconds)
+        {
+            IntervalInMilliseconds = intervalInMilliseconds;
+        }
+
+        public int IntervalInMilliseconds { get; }
+    }
+
+    internal class SetStrongholdPasswordClearIntervalCommandHandler : IRequestHandler<SetStrongholdPasswordClearIntervalCommand, IotaSDKResponse<bool>>
+    {
+        public SetStrongholdPasswordClearIntervalCommandHandler(RustBridgeWallet rustBridgeWallet)
+        {
+            
+        }
+        public Task<IotaSDKResponse<bool>> Handle(SetStrongholdPasswordClearIntervalCommand request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
