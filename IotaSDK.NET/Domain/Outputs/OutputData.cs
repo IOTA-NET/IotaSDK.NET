cef572e78951c96f853fef59562336418777032b
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-09 12:32:49 +0800

    Create OutputData.cs

---
+++ IotaSDK.NET/Domain/Outputs/OutputData.cs
@@ -0,0 +1,54 @@
+﻿using IotaSDK.NET.Domain.Addresses;
+
+namespace IotaSDK.NET.Domain.Outputs
+{
+    /// <summary>
+    /// An output with Metadata
+    /// </summary>
+    public class OutputData
+    {
+        public OutputData(
+            string outputId,
+            string networkId,
+            Address address,
+            Output output,
+            OutputMetadataResponse outputMetadata
+            )
+        {
+            OutputId = outputId;
+            NetworkId = networkId;
+            Address = address;
+            Output = output;
+            Metadata = outputMetadata;
+        }
+
+        /// <summary>
+        /// The identifier of an Output
+        /// </summary>
+        public string OutputId { get; set; }
+
+        /// <summary>
+        /// If an output is spent
+        /// </summary>
+        public bool IsSpent { get; set; }
+
+        public string NetworkId { get; set; }
+
+        public bool Remainder { get; set; }
+
+        /// <summary>
+        /// Associated account address
+        /// </summary>
+        public Address Address { get; set; }
+
+        /// <summary>
+        /// The actual Output
+        /// </summary>
+        public Output Output { get; set; }
+
+        /// <summary>
+        /// The metadata of the output
+        /// </summary>
+        public OutputMetadataResponse Metadata { get; set; }
+    }
+}
