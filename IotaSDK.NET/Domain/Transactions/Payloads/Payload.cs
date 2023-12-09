fbaa69f2c207f5eb07ce9b0d7815972ab8494f7e
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-09 10:54:01 +0800

    Create Payload.cs

---
+++ IotaSDK.NET/Domain/Transactions/Payloads/Payload.cs
@@ -0,0 +1,34 @@
+﻿using JsonSubTypes;
+using Newtonsoft.Json;
+using System;
+
+namespace IotaSDK.NET.Domain.Transactions.Payloads
+{
+    [JsonConverter(typeof(JsonSubtypes), "Type")]
+    [JsonSubtypes.KnownSubType(typeof(MilestonePayload), (int)PayloadType.Milestone)]
+    [JsonSubtypes.KnownSubType(typeof(TaggedDataPayload), (int)PayloadType.TaggedData)]
+    [JsonSubtypes.KnownSubType(typeof(TransactionPayload), (int)PayloadType.Transaction)]
+    [JsonSubtypes.KnownSubType(typeof(TreasuryTransactionPayload), (int)PayloadType.TreasuryTransaction)]
+    public abstract class Payload
+    {
+        public Payload(int type)
+        {
+            Type = type;
+        }
+
+        public int Type { get; }
+
+        public PayloadType GetPayloadType()
+        {
+            if (Enum.IsDefined(typeof(PayloadType), this.Type))
+            {
+                return (PayloadType)this.Type;
+            }
+            else
+            {
+                // Handle the case where the input value doesn't match any enum value
+                throw new ArgumentOutOfRangeException(nameof(this.Type), "Invalid PayloadType value");
+            }
+        }
+    }
+}
