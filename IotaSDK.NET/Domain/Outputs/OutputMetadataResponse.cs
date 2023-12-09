33c42dbfc3cfdca5b37c1cbb63816b4a7526af1a
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-09 12:32:57 +0800

    Create OutputMetadataResponse.cs

---
+++ IotaSDK.NET/Domain/Outputs/OutputMetadataResponse.cs
@@ -0,0 +1,54 @@
+﻿namespace IotaSDK.NET.Domain.Outputs
+{
+    public class OutputMetadataResponse
+    {
+
+        /**
+        * The block id the output was contained in.
+        */
+        public string? BlockId { get; set; }
+
+        /**
+         * The transaction id for the output.
+         */
+        public string? TransactionId { get; set; }
+
+        /**
+         * The index for the output.
+         */
+        public uint OutputIndex { get; set; }
+        /**
+         * Is the output spent.
+         */
+        public bool IsSpent { get; set; }
+        /**
+         * The milestone index at which this output was spent.
+         */
+        public uint MilestoneIndexSpent { get; set; }
+
+        /**
+         * The milestone timestamp this output was spent.
+         */
+        public uint MilestoneTimestampSpent { get; set; }
+
+        /**
+         * The transaction id for the output.
+         */
+        public string? TransactionIdSpent { get; set; }
+
+        /**
+         * The milestone index at which this output was booked into the ledger.
+         */
+        public uint MilestoneIndexBooked { get; set; }
+
+        /**
+         * The milestone timestamp this output was booked in the ledger.
+         */
+        public uint MilestoneTimestampBooked { get; set; }
+
+        /**
+         * The ledger index at which these output was available at.
+         */
+        public uint LedgerIndex { get; set; }
+    }
+}
