f116bc9b132111d1bb0d516857b2e3d2fb7af7c2
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-09 10:53:26 +0800

    Create PayloadType.cs

---
+++ IotaSDK.NET/Domain/Transactions/Payloads/PayloadType.cs
@@ -0,0 +1,17 @@
+﻿namespace IotaSDK.NET.Domain.Transactions.Payloads
+{
+    /// <summary>
+    /// All of the block payload types.
+    /// </summary>
+    public enum PayloadType
+    {
+        /** A milestone payload. */
+        Milestone = 7,
+        /** A tagged data payload. */
+        TaggedData = 5,
+        /** A transaction payload. */
+        Transaction = 6,
+        /** A treasury transaction payload. */
+        TreasuryTransaction = 4,
+    }
+}
