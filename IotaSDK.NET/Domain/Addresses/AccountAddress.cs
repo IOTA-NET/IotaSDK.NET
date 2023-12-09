c7cd4950bbb2d55323a64ed447b398bbcc6d1351
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-09 12:33:34 +0800

    Create AccountAddress.cs

---
+++ IotaSDK.NET/Domain/Addresses/AccountAddress.cs
@@ -0,0 +1,16 @@
+﻿namespace IotaSDK.NET.Domain.Addresses
+{
+    public class AccountAddress
+    {
+        public AccountAddress(string address)
+        {
+            Address = address;
+        }
+
+        public string Address { get; set; }
+        public int KeyIndex { get; set; }
+
+        public bool Used { get; set; }
+        public bool Internal { get; set; }
+    }
+}
