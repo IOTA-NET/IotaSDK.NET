886fa1deb0905020dbda3b9a5f2f2d587b0b91ae
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-09 10:50:08 +0800

    seperated input from inputtype

---
+++ IotaSDK.NET/Domain/Inputs/Input.cs
@@ -0,0 +1,32 @@
+﻿using JsonSubTypes;
+using Newtonsoft.Json;
+using System;
+
+namespace IotaSDK.NET.Domain.Inputs
+{
+    [JsonConverter(typeof(JsonSubtypes), "Type")]
+    [JsonSubtypes.KnownSubType(typeof(TreasuryInput), (int)InputType.Treasury)]
+    [JsonSubtypes.KnownSubType(typeof(UTXOInput), (int)InputType.UTXO)]
+    public abstract class Input
+    {
+        public Input(int type)
+        {
+            Type = type;
+        }
+
+        public int Type { get; }
+
+        public InputType GetInputType()
+        {
+            if (Enum.IsDefined(typeof(InputType), this.Type))
+            {
+                return (InputType)this.Type;
+            }
+            else
+            {
+                // Handle the case where the input value doesn't match any enum value
+                throw new ArgumentOutOfRangeException(nameof(this.Type), "Invalid InputType value");
+            }
+        }
+    }
+    }
--- IotaSDK.NET/Domain/Inputs/InputType.cs
+++ IotaSDK.NET/Domain/Inputs/InputType.cs
@@ -1,8 +1,5 @@
 ﻿using IotaSDK.NET.Domain.Outputs;
 using IotaSDK.NET.Domain.Transactions.EssenceTypes;
-using JsonSubTypes;
-using Newtonsoft.Json;
-using System;
 
 namespace IotaSDK.NET.Domain.Inputs
 {
@@ -12,31 +9,5 @@ namespace IotaSDK.NET.Domain.Inputs
         UTXO = 0,
         /** The treasury input. */
         Treasury = 1,
-    }
-
-    [JsonConverter(typeof(JsonSubtypes), "Type")]
-    [JsonSubtypes.KnownSubType(typeof(TreasuryInput), (int)InputType.Treasury)]
-    [JsonSubtypes.KnownSubType(typeof(UTXOInput), (int)InputType.UTXO)]
-    public abstract class Input
-    {
-        public Input(int type)
-        {
-            Type = type;
-        }
-
-        public int Type { get; }
-
-        public InputType GetInputType()
-        {
-            if (Enum.IsDefined(typeof(InputType), this.Type))
-            {
-                return (InputType)this.Type;
-            }
-            else
-            {
-                // Handle the case where the input value doesn't match any enum value
-                throw new ArgumentOutOfRangeException(nameof(this.Type), "Invalid InputType value");
-            }
-        }
     }
     }
