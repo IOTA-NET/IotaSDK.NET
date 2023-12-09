2299a18e4d875e0b3f2548283c1bae6e2cb1aee5
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-09 10:50:16 +0800

    Create TreasuryInput.cs

---
+++ IotaSDK.NET/Domain/Inputs/TreasuryInput.cs
@@ -0,0 +1,18 @@
+﻿namespace IotaSDK.NET.Domain.Inputs
+{
+    /// <summary>
+    /// A Treasury input
+    /// </summary>
+    public class TreasuryInput : Input
+    {
+        public TreasuryInput(string milestoneId) : base((int)InputType.Treasury)
+        {
+            MilestoneId = milestoneId;
+        }
+
+        /// <summary>
+        /// Hexencoded  milestone id of the input.
+        /// </summary>
+        public string MilestoneId { get; }
+    }
+}
