233e69844e7cb4ebfe0c750aeeccbf32bbc2b4e3
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-11 02:27:27 +0800

    Added getaccounts

---
+++ IotaSDK.NET/Contexts/WalletContext/Queries/GetAccountWithAlias/GetAccountWithAliasCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using IotaSDK.NET.Common.Exceptions;
+using IotaSDK.NET.Common.Interfaces;
+using IotaSDK.NET.Common.Rust;
+using IotaSDK.NET.Domain.Accounts;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IotaSDK.NET.Contexts.WalletContext.Queries.GetAccountWithAlias
+{
+    internal class GetAccountWithAliasCommandHandler : IRequestHandler<GetAccountWithAliasCommand, IotaSDKResponse<IAccount>>
+    {
+        private readonly RustBridgeWallet _rustBridgeWallet;
+
+        public GetAccountWithAliasCommandHandler(RustBridgeWallet rustBridgeWallet)
+        {
+            _rustBridgeWallet = rustBridgeWallet;
+        }
+        public async Task<IotaSDKResponse<IAccount>> Handle(GetAccountWithAliasCommand request, CancellationToken cancellationToken)
+        {
+            GetAccountWithAliasCommandModelData modelData = new GetAccountWithAliasCommandModelData(request.Alias);
+            IotaSDKModel<GetAccountWithAliasCommandModelData> model = new IotaSDKModel<GetAccountWithAliasCommandModelData>("getAccount", modelData);
+            string json = model.AsJson();
+
+            string? walletResponse =  await _rustBridgeWallet.CallWalletMethodAsync(request.WalletHandle, json);
+            
+            IotaSDKException.CheckForException(walletResponse!);
+
+            var accountMetaResponse =  IotaSDKResponse<AccountMeta>.CreateInstance(walletResponse);
+
+            IAccount account = new Account(accountMetaResponse.Payload.Index, accountMetaResponse.Payload.Alias);
+
+            IotaSDKResponse<IAccount> response = new IotaSDKResponse<IAccount>("getAccount");
+            response.Payload = account;
+
+            return response;    
+        }
+    }
+}
---
+++ IotaSDK.NET/Contexts/WalletContext/Queries/GetAccountWithAlias/GetAccountWithAliasCommandModelData.cs
@@ -0,0 +1,12 @@
+﻿namespace IotaSDK.NET.Contexts.WalletContext.Queries.GetAccountWithAlias
+{
+    internal class GetAccountWithAliasCommandModelData
+    {
+        public GetAccountWithAliasCommandModelData(string alias)
+        {
+            AccountId = alias;
+        }
+
+        public string AccountId { get; }
+    }
+}
