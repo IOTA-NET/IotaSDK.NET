6e722f5c1d562af81cb8baeaf225fc6c8fddbd04
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-12 22:08:33 +0800

    Added get accounts

---
+++ IotaSDK.NET/Contexts/WalletContext/Queries/GetAccounts/GetAccountsQuery.cs
@@ -0,0 +1,13 @@
+﻿using IotaSDK.NET.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace IotaSDK.NET.Contexts.WalletContext.Queries.GetAccounts
+{
+    internal class GetAccountsQuery : WalletRequest<IotaSDKResponse<List<IAccount>>>
+    {
+        public GetAccountsQuery(IntPtr walletHandle) : base(walletHandle)
+        {
+        }
+    }
+}
