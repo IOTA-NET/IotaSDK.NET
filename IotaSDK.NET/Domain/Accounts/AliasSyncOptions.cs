c7d0a251ce94acee4ec0b974156e26e7ffad81e8
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-13 21:46:53 +0800

    Create AliasSyncOptions.cs

---
+++ IotaSDK.NET/Domain/Accounts/AliasSyncOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace IotaSDK.NET.Domain.Accounts
+{
+    /// <summary>
+    /// Specifies what outputs should be synced for the address of an alias output
+    /// </summary>
+    public class AliasSyncOptions : AccountSyncOptions
+    {
+
+        /// <summary>
+        ///  Whether to sync foundry outputs.
+        /// </summary>
+        public bool? FoundryOutputs { get; set; }
+    }
+}
