98a2be3bb4db218ce6d90c71632f67e7df217c4c
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-17 07:32:17 +0800

    Create PreparedTransactionData.cs

---
+++ IotaSDK.NET/Domain/Transactions/Prepared/PreparedTransactionData.cs
@@ -0,0 +1,29 @@
+﻿using IotaSDK.NET.Domain.Inputs;
+using IotaSDK.NET.Domain.Outputs;
+using IotaSDK.NET.Domain.Transactions.EssenceTypes;
+using System.Collections.Generic;
+
+namespace IotaSDK.NET.Domain.Transactions.Prepared
+{
+    public class PreparedTransactionData
+    {
+        // Transaction essence
+        public TransactionEssence Essence { get; set; }
+
+        // Required address information for signing
+        public List<InputSigningData> InputsData { get; set; }
+
+        // Optional remainder output information
+        public Remainder? Remainder { get; set; }
+
+        // Constructor with default values
+        public PreparedTransactionData(TransactionEssence essence, List<InputSigningData> inputsData, Remainder? remainder = null)
+        {
+            Essence = essence;
+            InputsData = inputsData;
+            Remainder = remainder;
+        }
+    }
+
+
+}
