845431636772e8c82fcb5746c723fb5576422699
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-19 22:43:33 +0800

    Added PrepareConsolidation

---
+++ IotaSDK.NET/Contexts/AccountContext/Commands/PrepareConsolidateOutputs/PrepareConsolidateOutputsCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using IotaSDK.NET.Common.Exceptions;
+using IotaSDK.NET.Common.Models;
+using IotaSDK.NET.Common.Rust;
+using IotaSDK.NET.Domain.Transactions.Prepared;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IotaSDK.NET.Contexts.AccountContext.Commands.PrepareConsolidateOutputs
+{
+    internal class PrepareConsolidateOutputsCommandHandler : IRequestHandler<PrepareConsolidateOutputsCommand, IotaSDKResponse<PreparedTransactionData>>
+    {
+        private readonly RustBridgeWallet _rustBridgeWallet;
+        private readonly IotaSDKAccountModelCreator<PrepareConsolidateOutputsCommandModelData> _iotaSDKAccountModelCreator;
+
+        public PrepareConsolidateOutputsCommandHandler(RustBridgeWallet rustBridgeWallet, IotaSDKAccountModelCreator<PrepareConsolidateOutputsCommandModelData> iotaSDKAccountModelCreator)
+        {
+            _rustBridgeWallet = rustBridgeWallet;
+            _iotaSDKAccountModelCreator = iotaSDKAccountModelCreator;
+        }
+        public async Task<IotaSDKResponse<PreparedTransactionData>> Handle(PrepareConsolidateOutputsCommand request, CancellationToken cancellationToken)
+        {
+            var accountModel = _iotaSDKAccountModelCreator.Create("prepareConsolidateOutputs", request.AccountIndex, new PrepareConsolidateOutputsCommandModelData(request.ConsolidationOptions));
+            string json = accountModel.AsJson();
+
+            string? accountResponse = await _rustBridgeWallet.CallWalletMethodAsync(request.WalletHandle, json);
+
+            IotaSDKException.CheckForException(accountResponse!);
+
+            return IotaSDKResponse<PreparedTransactionData>.CreateInstance(accountResponse);
+        }
+    }
+}
