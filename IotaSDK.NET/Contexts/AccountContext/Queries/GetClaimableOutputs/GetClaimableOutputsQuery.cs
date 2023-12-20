0d4687a5a83db677d4fbdbb8196f826f93af6016
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-20 21:00:47 +0800

    Added GetClaimableOutputs

---
+++ IotaSDK.NET/Contexts/AccountContext/Queries/GetClaimableOutputs/GetClaimableOutputsQuery.cs
@@ -0,0 +1,18 @@
+﻿using IotaSDK.NET.Common.Interfaces;
+using IotaSDK.NET.Common.Models;
+using IotaSDK.NET.Domain.Outputs;
+using System;
+using System.Collections.Generic;
+
+namespace IotaSDK.NET.Contexts.AccountContext.Queries.GetClaimableOutputs
+{
+    internal class GetClaimableOutputsQuery : AccountRequest<IotaSDKResponse<List<string>>>
+    {
+        public GetClaimableOutputsQuery(IntPtr walletHandle, int accountIndex, ClaimableOutputType claimableOutputType) : base(walletHandle, accountIndex)
+        {
+            ClaimableOutputType = claimableOutputType;
+        }
+
+        public ClaimableOutputType ClaimableOutputType { get; }
+    }
+}
