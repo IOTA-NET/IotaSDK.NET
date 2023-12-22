d3a9c226b8dd6e94c029928c78e94d60ab4d66f5
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-23 00:41:29 +0800

    Create BigIntJsonConverter.cs

---
+++ IotaSDK.NET/Common/Serializers/BigIntJsonConverter.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Numerics;
+
+namespace IotaSDK.NET.Common.Serializers
+{
+    internal class BigIntJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            List<Type> validTypes = new List<Type>() { typeof(int), typeof(uint), typeof(long), typeof(ulong), typeof(string), typeof(BigInteger) };
+            
+            if(validTypes.Contains(objectType))
+                return true;
+            return false;
+        }
+
+        public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+        {
+            var t = reader.ValueType;
+            var token = reader.Value as string;
+
+            if (token == null)
+                return null;
+
+            if (token.ToLower().StartsWith("0x"))
+                token = token.Substring(2);
+
+            return BigInteger.Parse(token, NumberStyles.HexNumber);
+
+        }
+
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+        {
+            if (value == null)
+                return;
+
+            writer.WriteValue($"0x{(BigInteger)value:X}");
+        }
+    }
+}
