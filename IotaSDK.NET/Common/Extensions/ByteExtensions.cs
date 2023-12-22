feb6121b3c4c6af6411546de8015ffd88eb95631
wireless90 <seewhatseeabc@gmail.com>
2023-12-22 11:16:12 +0800

    Create ByteExtensions.cs

---
+++ IotaSDK.NET/Common/Extensions/ByteExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IotaSDK.NET.Common.Extensions
+{
+    public static class ByteExtensions
+    {
+        public static string ToHexString(this byte[] bytes)
+        {
+            return "0x" + BitConverter.ToString(bytes).Replace("-", "");
+        }
+
+        public static byte[] FromHexString(this string hexString)
+        {
+            hexString = hexString.StartsWith("0x", StringComparison.OrdinalIgnoreCase) ? hexString.Substring(2) : hexString;
+
+            byte[] bytes = new byte[hexString.Length / 2];
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+            }
+
+            return bytes;
+        }
+    }
+
+}
