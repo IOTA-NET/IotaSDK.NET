3a88191580400f20b9574e3731df0c36834392fb
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-24 15:18:13 +0800

    Added RequestFundsFromfaucet for account

---
+++ IotaSDK.NET/Contexts/AccountContext/Commands/RequestFundsFromFaucet/RequestFundsFromFaucetCommand.cs
@@ -0,0 +1,22 @@
+﻿using IotaSDK.NET.Common.Interfaces;
+using IotaSDK.NET.Common.Models;
+using IotaSDK.NET.Domain.Faucet;
+using System;
+
+namespace IotaSDK.NET.Contexts.AccountContext.Commands.RequestFundsFromFaucet
+{
+    internal class RequestFundsFromFaucetCommand : AccountRequest<IotaSDKResponse<FaucetResponse>>
+    {
+        public RequestFundsFromFaucetCommand(IntPtr walletHandle, int accountIndex, IClient client, IAccount account, string? bech32Address=null) 
+            : base(walletHandle, accountIndex)
+        {
+            Client = client;
+            Account = account;
+        }
+
+
+        public string? Bech32Address { get; }
+        public IClient Client { get; }
+        public IAccount Account { get; }
+    }
+}
---
+++ IotaSDK.NET/Contexts/AccountContext/Commands/RequestFundsFromFaucet/RequestFundsFromFaucetCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using IotaSDK.NET.Common.Models;
+using IotaSDK.NET.Domain.Faucet;
+using MediatR;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IotaSDK.NET.Contexts.AccountContext.Commands.RequestFundsFromFaucet
+{
+    internal class RequestFundsFromFaucetCommandHandler : IRequestHandler<RequestFundsFromFaucetCommand, IotaSDKResponse<FaucetResponse>>
+    {
+
+        public RequestFundsFromFaucetCommandHandler()
+        {
+        }
+        public async Task<IotaSDKResponse<FaucetResponse>> Handle(RequestFundsFromFaucetCommand request, CancellationToken cancellationToken)
+        {
+            string? bech32Address = request.Bech32Address;
+            
+            if(bech32Address == null)
+            {
+                bech32Address = (await request.Account.GetAddressesAsync()).Payload.First().Address;
+            }
+
+            return await request.Client.RequestFundsFromFaucetAsync(bech32Address);
+
+        }
+    }
+}
