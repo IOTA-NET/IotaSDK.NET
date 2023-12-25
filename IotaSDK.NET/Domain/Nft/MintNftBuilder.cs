19cb2157b3c47a587125746731f4d203de8af12b
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-25 22:15:06 +0800

    Create MintNftBuilder.cs

---
+++ IotaSDK.NET/Domain/Nft/MintNftBuilder.cs
@@ -0,0 +1,38 @@
+﻿using IotaSDK.NET.Common.Exceptions;
+using IotaSDK.NET.Common.Interfaces;
+using IotaSDK.NET.Common.Models;
+using IotaSDK.NET.Domain.Transactions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IotaSDK.NET.Domain.Nft
+{
+    public class MintNftBuilder
+    {
+        private readonly IAccount _account;
+        private NftOptionsBuilder _optionsBuilder;
+        private List<NftOptions> _options;
+
+        public MintNftBuilder(IAccount account)
+        {
+            _options = new List<NftOptions>();
+            _account = account;
+        }
+
+        public NftOptionsBuilder CreateNewNft()
+        {
+            _optionsBuilder = new NftOptionsBuilder(this, x => _options.Add(x));
+            return _optionsBuilder;
+        }
+
+
+        public async Task<IotaSDKResponse<Transaction>> MinftNftsAsync(TransactionOptions? transactionOptions = null)
+        {
+            if (_options.Any() == false)
+                throw new IotaSDKException("No nft created. Do use 'CreateNewNft' method");
+
+            return await _account.MintNftsAsync(_options, transactionOptions);
+        }
+    }
+}
