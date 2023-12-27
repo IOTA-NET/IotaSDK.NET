3a69ee7ee80146245418e3ed8b288090cccf64ba
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-27 10:40:03 +0800

    Added PrepareMintNativeTokens

---
+++ IotaSDK.NET/Contexts/AccountContext/Commands/PrepareMintNativeTokens/PrepareMintNativeTokensCommand.cs
@@ -0,0 +1,28 @@
+﻿using IotaSDK.NET.Common.Interfaces;
+using IotaSDK.NET.Common.Models;
+using IotaSDK.NET.Common.Serializers;
+using IotaSDK.NET.Domain.Transactions;
+using IotaSDK.NET.Domain.Transactions.Prepared;
+using Newtonsoft.Json;
+using System;
+using System.Numerics;
+
+namespace IotaSDK.NET.Contexts.AccountContext.Commands.PrepareMintNativeTokens
+{
+    internal class PrepareMintNativeTokensCommand : AccountRequest<IotaSDKResponse<PreparedTransactionData>>
+    {
+        public PrepareMintNativeTokensCommand(IntPtr walletHandle, int accountIndex, string tokenId, BigInteger numberOfTokensToMint, TransactionOptions? transactionOptions=null) : base(walletHandle, accountIndex)
+        {
+            TokenId = tokenId;
+            NumberOfTokensToMint = numberOfTokensToMint;
+            TransactionOptions = transactionOptions;
+        }
+
+        public string TokenId { get; }
+
+        [JsonConverter(typeof(BigIntJsonConverter))]
+        public BigInteger NumberOfTokensToMint { get; }
+
+        public TransactionOptions? TransactionOptions { get; }
+    }
+}
---
+++ IotaSDK.NET/Contexts/AccountContext/Commands/PrepareMintNativeTokens/PrepareMintNativeTokensCommandModelData.cs
@@ -0,0 +1,25 @@
+﻿using IotaSDK.NET.Common.Serializers;
+using IotaSDK.NET.Domain.Transactions;
+using Newtonsoft.Json;
+using System.Numerics;
+
+namespace IotaSDK.NET.Contexts.AccountContext.Commands.PrepareMintNativeTokens
+{
+    internal class PrepareMintNativeTokensCommandModelData
+    {
+
+        public PrepareMintNativeTokensCommandModelData(string tokenId, BigInteger mintAmount, TransactionOptions? options = null)
+        {
+            TokenId = tokenId;
+            MintAmount = mintAmount;
+            Options = options;
+        }
+
+        public string TokenId { get; }
+
+        [JsonConverter(typeof(BigIntJsonConverter))]
+        public BigInteger MintAmount { get; }
+
+        public TransactionOptions? Options { get; }
+    }
+}
