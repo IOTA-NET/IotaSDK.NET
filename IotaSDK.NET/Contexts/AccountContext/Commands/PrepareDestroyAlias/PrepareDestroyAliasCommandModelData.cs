197a5c6843a5447fe17ac4da1a358d4e1d13ff58
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-28 19:10:58 +0800

    Create PrepareDestroyAliasCommandModelData.cs

---
+++ IotaSDK.NET/Contexts/AccountContext/Commands/PrepareDestroyAlias/PrepareDestroyAliasCommandModelData.cs
@@ -0,0 +1,16 @@
+﻿using IotaSDK.NET.Domain.Transactions;
+
+namespace IotaSDK.NET.Contexts.AccountContext.Commands.PrepareDestroyAlias
+{
+    internal class PrepareDestroyAliasCommandModelData
+    {
+        public PrepareDestroyAliasCommandModelData(PrepareDestroyAliasCommandInnerModelData burn, TransactionOptions? options)
+        {
+            Burn = burn;
+            Options = options;
+        }
+
+        public PrepareDestroyAliasCommandInnerModelData Burn { get; }
+        public TransactionOptions? Options { get; }
+    }
+}
