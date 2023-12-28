b0863437fd174452304bfde41e5d366191e8cf06
wireless90 <12537739+wireless90@users.noreply.github.com>
2023-12-28 19:24:00 +0800

    code cleanup

--- IotaSDK.NET/Contexts/AccountContext/Commands/PrepareDestroyAlias/PrepareDestroyAliasCommand.cs
+++ IotaSDK.NET/Contexts/AccountContext/Commands/PrepareDestroyAlias/PrepareDestroyAliasCommand.cs
@@ -1,20 +1,14 @@
-﻿using IotaSDK.NET.Common.Exceptions;
-using IotaSDK.NET.Common.Interfaces;
+﻿using IotaSDK.NET.Common.Interfaces;
 using IotaSDK.NET.Common.Models;
-using IotaSDK.NET.Common.Rust;
 using IotaSDK.NET.Domain.Transactions;
 using IotaSDK.NET.Domain.Transactions.Prepared;
-using MediatR;
 using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace IotaSDK.NET.Contexts.AccountContext.Commands.PrepareDestroyAlias
 {
     internal class PrepareDestroyAliasCommand : AccountRequest<IotaSDKResponse<PreparedTransactionData>>
     {
-        public PrepareDestroyAliasCommand(IntPtr walletHandle, int accountIndex, string aliasId, TransactionOptions? transactionOptions=null) 
+        public PrepareDestroyAliasCommand(IntPtr walletHandle, int accountIndex, string aliasId, TransactionOptions? transactionOptions = null)
             : base(walletHandle, accountIndex)
         {
             AliasId = aliasId;
