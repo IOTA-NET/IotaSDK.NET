f372cde42f6500356c14c29033ceebb40929390b
wireless90 <12537739+wireless90@users.noreply.github.com>
2024-01-01 22:47:19 +0800

    Added BurnNativeTokens

---
+++ IotaSDK.NET/Contexts/AccountContext/Commands/BurnNativeTokens/BurnNativeTokensCommand.cs
@@ -0,0 +1,23 @@
+﻿using IotaSDK.NET.Common.Interfaces;
+using IotaSDK.NET.Common.Models;
+using IotaSDK.NET.Domain.Transactions;
+using System;
+using System.Numerics;
+
+namespace IotaSDK.NET.Contexts.AccountContext.Commands.BurnNativeTokens
+{
+    internal class BurnNativeTokensCommand : AccountRequest<IotaSDKResponse<Transaction>>
+    {
+        public BurnNativeTokensCommand(IntPtr walletHandle, int accountIndex, string tokenId, BigInteger numberOfTokensToBurn, TransactionOptions? transactionOptions = null)
+            : base(walletHandle, accountIndex)
+        {
+            TokenId = tokenId;
+            NumberOfTokensToBurn = numberOfTokensToBurn;
+            TransactionOptions = transactionOptions;
+        }
+
+        public string TokenId { get; }
+        public BigInteger NumberOfTokensToBurn { get; }
+        public TransactionOptions? TransactionOptions { get; }
+    }
+}
---
+++ IotaSDK.NET/Contexts/AccountContext/Commands/BurnNativeTokens/BurnNativeTokensCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using IotaSDK.NET.Common.Models;
+using IotaSDK.NET.Contexts.AccountContext.Commands.PrepareBurnNativeTokens;
+using IotaSDK.NET.Contexts.AccountContext.Commands.SignAndSubmitTransaction;
+using IotaSDK.NET.Domain.Transactions;
+using IotaSDK.NET.Domain.Transactions.Prepared;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IotaSDK.NET.Contexts.AccountContext.Commands.BurnNativeTokens
+{
+    internal class BurnNativeTokensCommandHandler : IRequestHandler<BurnNativeTokensCommand, IotaSDKResponse<Transaction>>
+    {
+        private readonly IMediator _mediator;
+
+        public BurnNativeTokensCommandHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        public async Task<IotaSDKResponse<Transaction>> Handle(BurnNativeTokensCommand request, CancellationToken cancellationToken)
+        {
+            var preparedTransactionResponse = await _mediator.Send(new PrepareBurnNativeTokensCommand(request.WalletHandle, request.AccountIndex, request.TokenId, request.NumberOfTokensToBurn, request.TransactionOptions));
+            PreparedTransactionData preparedTransactionData = preparedTransactionResponse.Payload;
+
+            return await _mediator.Send(new SignAndSubmitTransactionCommand(request.WalletHandle, request.AccountIndex, preparedTransactionData));
+        }
+    }
+}
