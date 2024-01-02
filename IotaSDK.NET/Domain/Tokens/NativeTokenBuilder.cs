d38640b5f883130a51f854f57b59f50aae6c2ac2
wireless90 <12537739+wireless90@users.noreply.github.com>
2024-01-02 22:37:00 +0800

    Added builders for creation of native tokens

---
+++ IotaSDK.NET/Domain/Tokens/NativeTokenBuilder.cs
@@ -0,0 +1,61 @@
+﻿using IotaSDK.NET.Common.Interfaces;
+using IotaSDK.NET.Common.Models;
+using IotaSDK.NET.Domain.Options;
+using IotaSDK.NET.Domain.Transactions;
+using System;
+using System.Threading.Tasks;
+
+namespace IotaSDK.NET.Domain.Tokens
+{
+    public class NativeTokenBuilder
+    {
+        private NativeTokenIrc30Builder _nativeTokenIrc30Builder;
+        private NativeTokenIrc30? _nativeTokenIrc30;
+        private NativeToken? _nativeToken;
+        private ulong? _circulatingSupply, _maximumSupply;
+        private string? _aliasId;
+        private readonly IAccount _account;
+
+        public NativeTokenBuilder(IAccount account)
+        {
+            _nativeTokenIrc30Builder = new NativeTokenIrc30Builder(this, x => _nativeTokenIrc30=x);
+            _circulatingSupply = null;
+            _maximumSupply = null;
+            _aliasId = null;
+            _account = account;
+        }
+
+        public NativeTokenIrc30Builder SetFoundryMetadata()
+        {
+            return _nativeTokenIrc30Builder;
+        }
+
+        public NativeTokenBuilder SetCirculatingSupply(ulong circulatingSupply)
+        {
+            _circulatingSupply= circulatingSupply;
+            return this;
+        }
+
+        public NativeTokenBuilder SetMaximumSupply(ulong maximumSupply)
+        {
+            _maximumSupply= maximumSupply;
+            return this;
+        }
+
+        public NativeTokenBuilder SetAliasId(string aliasId)
+        {
+            _aliasId = aliasId;
+            return this;    
+        }
+
+        public async Task<IotaSDKResponse<Transaction>> CreateNativeTokensAsync(TransactionOptions? transactionOptions)
+        {
+            if (_circulatingSupply == null || _maximumSupply == null)
+                throw new ArgumentNullException("Set both Circulating supply and Maximum supply");
+
+            NativeTokenCreationOptions creationOptions = new NativeTokenCreationOptions(_circulatingSupply.Value, _maximumSupply.Value, _nativeTokenIrc30, _aliasId);
+            return await _account.CreateNativeTokenAsync(creationOptions, transactionOptions);
+        }
+
+    }
+}
---
+++ IotaSDK.NET/Domain/Tokens/NativeTokenIrc30Builder.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace IotaSDK.NET.Domain.Tokens
+{
+    public class NativeTokenIrc30Builder
+    {
+        private string? _name, _symbol, _logoUrl, _description, _resourceUrl;
+        private uint _decimals;
+        private readonly NativeTokenBuilder _nativeTokenBuilder;
+        private readonly Action<NativeTokenIrc30> _setNativeTokenIrc30;
+
+        public NativeTokenIrc30Builder(NativeTokenBuilder nativeTokenBuilder, Action<NativeTokenIrc30> setNativeTokenIrc30)
+        {
+            _nativeTokenBuilder = nativeTokenBuilder;
+            _setNativeTokenIrc30 = setNativeTokenIrc30;
+        }
+
+        public NativeTokenIrc30Builder SetTokenName(string tokenName)
+        {
+            _name = tokenName;
+            return this;
+        }
+
+        public NativeTokenIrc30Builder SetSymbol(string symbol)
+        {
+            _symbol = symbol;
+            return this;
+        }
+
+        public NativeTokenIrc30Builder SetLogoUrl(string logoUrl)
+        {
+            _logoUrl = logoUrl;
+            return this;
+        }
+
+        public NativeTokenIrc30Builder SetDescription(string description)
+        {
+            _description = description;
+            return this;
+        }
+
+        public NativeTokenIrc30Builder SetResourceUrl(string resourceUrl)
+        {
+            _resourceUrl = resourceUrl;
+            return this;
+        }
+
+        public NativeTokenIrc30Builder SetNumberOfDecimals(uint decimals)
+        {
+            _decimals = decimals;
+            return this;
+        }
+
+        public NativeTokenBuilder Then()
+        {
+            if(_name == null || _symbol == null)
+            {
+                throw new ArgumentNullException("Ensure to set Token name, Symbol and decimal");
+            }
+
+            NativeTokenIrc30 nativeTokenIrc30 = new NativeTokenIrc30(_name!, _symbol!, _decimals);
+            nativeTokenIrc30.LogoUrl = _logoUrl;
+            nativeTokenIrc30.Description = _description;
+            nativeTokenIrc30.Url = _resourceUrl;
+            
+            _setNativeTokenIrc30(nativeTokenIrc30);
+
+            return _nativeTokenBuilder;
+        }
+
+    }
+}
