3004e5e3cdc1d000e7583361073ceb6664bbbbd0
wireless90 <12537739+wireless90@users.noreply.github.com>
2024-01-04 10:52:42 +0800

    Added restorebackup

---
+++ IotaSDK.NET/Contexts/WalletContext/Commands/RestoreBackup/RestoreBackupCommand.cs
@@ -0,0 +1,23 @@
+﻿using IotaSDK.NET.Common.Interfaces;
+using IotaSDK.NET.Common.Models;
+using System;
+
+namespace IotaSDK.NET.Contexts.WalletContext.Commands.RestoreBackup
+{
+    internal class RestoreBackupCommand : WalletRequest<IotaSDKResponse<bool>>
+    {
+        public RestoreBackupCommand(IntPtr walletHandle, string sourcePath, string password, bool ignoreIfCoinTypeMismatch=true, bool ignoreIfBech32Mismatch=true) 
+            : base(walletHandle)
+        {
+            SourcePath = sourcePath;
+            Password = password;
+            IgnoreIfCoinTypeMismatch = ignoreIfCoinTypeMismatch;
+            IgnoreIfBech32Mismatch = ignoreIfBech32Mismatch;
+        }
+
+        public string SourcePath { get; }
+        public string Password { get; }
+        public bool IgnoreIfCoinTypeMismatch { get; }
+        public bool IgnoreIfBech32Mismatch { get; }
+    }
+}
---
+++ IotaSDK.NET/Contexts/WalletContext/Commands/RestoreBackup/RestoreBackupCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using IotaSDK.NET.Common.Exceptions;
+using IotaSDK.NET.Common.Models;
+using IotaSDK.NET.Common.Rust;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IotaSDK.NET.Contexts.WalletContext.Commands.RestoreBackup
+{
+    internal class RestoreBackupCommandHandler : IRequestHandler<RestoreBackupCommand, IotaSDKResponse<bool>>
+    {
+        private readonly RustBridgeWallet _rustBridgeWallet;
+
+        public RestoreBackupCommandHandler(RustBridgeWallet rustBridgeWallet)
+        {
+            _rustBridgeWallet = rustBridgeWallet;
+        }
+        public async Task<IotaSDKResponse<bool>> Handle(RestoreBackupCommand request, CancellationToken cancellationToken)
+        {
+            RestoreBackupCommandModel model = new RestoreBackupCommandModel(request.SourcePath, request.Password, request.IgnoreIfCoinTypeMismatch, request.IgnoreIfBech32Mismatch);
+            IotaSDKModel<RestoreBackupCommandModel> iotaSDKModel = new IotaSDKModel<RestoreBackupCommandModel>("restoreBackup", model);
+            string json = iotaSDKModel.AsJson();
+
+            string? walletResponse = await _rustBridgeWallet.CallWalletMethodAsync(request.WalletHandle, json);
+
+            IotaSDKException.CheckForException(walletResponse!);
+
+            var response = IotaSDKResponse<bool>.CreateInstance(walletResponse, type: "restoreBackup");
+            response.Payload = true;
+
+            return response;
+        }
+    }
+}
